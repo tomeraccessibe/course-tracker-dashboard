789b86618f8d0aabf84037b72421c53f0c6c359d
tomercarolla <tomerca@accessibe.com>
2024-12-22 15:01:22 +0200

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
-
 const App = () => (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </Router>
+    <Index />
 );
 
 export default App;
