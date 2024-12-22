143aa9132cb99b86eb198d171d4ebae3e456ff91
tomercarolla <tomerca@accessibe.com>
2024-12-22 14:48:16 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
 
 const App = () => (
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
 );
 
 export default App;
